30a912a90075203829fb1c416d6c8505e0d2f26c
Zied <zinzied@gmail.com>
2026-01-30 12:09:03 +0100

    chore: Bump package version to 3.8.1.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name = 'ai-cloudscraper',
     author='Zied Boughdir',
     author_email='zinzied@gmail.com',
-    version='3.8.0',
+    version='3.8.1',
     packages = ['cloudscraper', 'cloudscraper.captcha', 'cloudscraper.interpreters', 'cloudscraper.user_agent'],
     ext_modules = [],  # No Cython compilation needed
     py_modules = [],
